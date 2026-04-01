086401923dba38c0d19abe2cce5f1685b658f368
Alwisteins <alwi.wahyu.w@gmail.com>
2026-04-01 12:44:36 +0700

    feat: complete test

---
+++ src/main/java/com/alwisteins/datapribadi/DatapribadiApplication.java
@@ -0,0 +1,13 @@
+package com.alwisteins.datapribadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatapribadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatapribadiApplication.class, args);
+	}
+
+}
